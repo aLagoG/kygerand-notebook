7aae42ba90e035ab9e92fefebf00e4e02dc3348b
Andres de Lago Gomez <delagoandres@gmail.com>
2018-03-07 21:15:16 -0600

    Add another method for LIS nLogN

--- dynamic_programming/LIS/nLogN.cpp
+++ dynamic_programming/LIS/nLogN.cpp
@@ -3,13 +3,14 @@ reference:
 http://www.techiedelight.com/longest-increasing-subsequence/
 https://stackoverflow.com/questions/2631726/how-to-determine-the-longest-increasing-subsequence-using-dynamic-programming
 */
+#include <algorithm>
 #include <set>
 #include <vector>
 
 using namespace std;
 
 // returns the length of the LIS
-int LISLength(vector<int> &v) {
+int setLISLength(vector<int> &v) {
     int n = v.size();
     set<int> s;
     for (int i = 0; i < n; i++) {
@@ -22,4 +23,25 @@ int LISLength(vector<int> &v) {
         }
     }
     return s.size();
+}
+
+int bsLISLength(vector<int> &v) {
+    int n = v.size();
+    if (!n) {
+        return 0;
+    }
+    vector<int> tail(n);
+    tail[0] = v[0];
+    int length = 1;
+    for (int i = 1; i < n; i++) {
+        if (v[i] < tail[0]) {
+            tail[0] = v[i];
+        } else if (v[i] > tail[length - 1]) {
+            tail[length++] = v[i];
+        } else {
+            auto it = lower_bound(tail.begin(), tail.begin() + length, v[i]);
+            *it = v[i];
+        }
+    }
+    return length;
 }
--- dynamic_programming/LIS/nLogN_test.cpp
+++ dynamic_programming/LIS/nLogN_test.cpp
@@ -1,33 +1,39 @@
 #include "catch.hpp"
 
-#include "./nLogN.h"
+#include "nLogN.h"
 
 TEST_CASE("1 2 1 4", "[LIS]") {
     vector<int> v{1, 2, 1, 4};
-    REQUIRE(LISLength(v) == 3);
+    REQUIRE(setLISLength(v) == 3);
+    REQUIRE(bsLISLength(v) == 3);
 }
 
 TEST_CASE("1 2 3 3 4", "[LIS]") {
     vector<int> v{1, 2, 3, 3, 4};
-    REQUIRE(LISLength(v) == 4);
+    REQUIRE(setLISLength(v) == 4);
+    REQUIRE(bsLISLength(v) == 4);
 }
 
 TEST_CASE("1", "[LIS]") {
     vector<int> v{1};
-    REQUIRE(LISLength(v) == 1);
+    REQUIRE(setLISLength(v) == 1);
+    REQUIRE(bsLISLength(v) == 1);
 }
 
 TEST_CASE("-1 1", "[LIS]") {
     vector<int> v{-1, 1};
-    REQUIRE(LISLength(v) == 2);
+    REQUIRE(setLISLength(v) == 2);
+    REQUIRE(bsLISLength(v) == 2);
 }
 
 TEST_CASE("empty", "[LIS]") {
     vector<int> v{};
-    REQUIRE(LISLength(v) == 0);
+    REQUIRE(setLISLength(v) == 0);
+    REQUIRE(bsLISLength(v) == 0);
 }
 
-TEST_CASE("{0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15}", "[LIS]"){
+TEST_CASE("{0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15}", "[LIS]") {
     vector<int> v{0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
-    REQUIRE(LISLength(v) == 6);
+    REQUIRE(setLISLength(v) == 6);
+    REQUIRE(bsLISLength(v) == 6);
 }
